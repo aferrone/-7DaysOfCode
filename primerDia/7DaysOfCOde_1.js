4bf639501ba4ad3a59fbf1c0c4503dbbf8eeb80f
sisifo88 <73007547+aferrone@users.noreply.github.com>
2024-08-15 13:33:29 -0300

    Add files via upload

---
+++ primerDia/7DaysOfCOde_1.js
@@ -0,0 +1,26 @@
+
+
+let numeroUn = 1;
+let stringUn = "1";
+let numeroTreinta = 30;
+let stringTreinta = "30";
+let numeroDiez = 10;
+let stringDiez = "10";
+
+if (numeroUn === stringUn) {
+    console.log("Error en la logica, un numero nunca es igual a una cadena")
+} else {
+    console.log("Las variables numeroUn y stringUn no tienen el mismo valor, ni el mismo tipo")
+}
+
+if (numeroTreinta === stringTreinta) {
+    console.log("Error en la logica, un numero nunca es igual a una cadena")
+} else {
+    console.log("Las variables numeroTreinta y stringTreinta no tienen el mismo valor ni son el mismo tipo")
+}
+
+if (numeroDiez === stringDiez) {
+    console.log ("Error en la logica, un numero nunca es igual a una cadena")
+} else {
+    console.log("Las variables numeroDiez y stringDiez no tienen el mismo valor ni el mismo tipo")
+}
