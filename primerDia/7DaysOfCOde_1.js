8eeda446d896b9fadf452943e0a1ddf7cd34868b
sisifo88 <73007547+aferrone@users.noreply.github.com>
2024-08-15 14:06:29 -0300

    Update 7DaysOfCOde_1.js

--- primerDia/7DaysOfCOde_1.js
+++ primerDia/7DaysOfCOde_1.js
@@ -1,5 +1,3 @@
-
-
 let numeroUn = 1;
 let stringUn = "1";
 let numeroTreinta = 30;
@@ -7,20 +5,25 @@ let stringTreinta = "30";
 let numeroDiez = 10;
 let stringDiez = "10";
 
-if (numeroUn === stringUn) {
-    console.log("Error en la logica, un numero nunca es igual a una cadena")
+if (numeroUn == stringUn) {
+    console.log("Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes")
 } else {
-    console.log("Las variables numeroUn y stringUn no tienen el mismo valor, ni el mismo tipo")
+    console.log("Las variables numeroUn y stringUn no tienen el mismo valor.")
 }
 
 if (numeroTreinta === stringTreinta) {
-    console.log("Error en la logica, un numero nunca es igual a una cadena")
+    console.log("Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo")
 } else {
-    console.log("Las variables numeroTreinta y stringTreinta no tienen el mismo valor ni son el mismo tipo")
+    console.log("Las variables numeroTreinta y stringTreinta no tienen el mismo tipo")
 }
 
-if (numeroDiez === stringDiez) {
-    console.log ("Error en la logica, un numero nunca es igual a una cadena")
+if (numeroDiez == stringDiez) {
+    console.log ("Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes.")
 } else {
-    console.log("Las variables numeroDiez y stringDiez no tienen el mismo valor ni el mismo tipo")
+    console.log("Las variables numeroDiez y stringDiez no tienen el mismo valor")
 }
+
+
+
+
+
