c4e66800433a8803f3f40d92c1ec330c8750183b
sisifo88 <73007547+aferrone@users.noreply.github.com>
2024-08-16 13:27:09 -0300

    segundo reto de 7DaysOfCode

---
+++ segundoDia/7DaysOfCode_2.js
@@ -0,0 +1,17 @@
+const nombre = prompt("Cual es tu nombre?: ");
+const edad = prompt("Cual es tu edad?: ");
+const lenguaje = prompt("Que lenguaje de programacion estas estudiando?: ");
+
+console.log (`Hola ${nombre}, tienes ${edad} años y ya estas aprendiendo ${lenguaje}!`);
+
+interaccion1 =parseInt(prompt(`Te gusta estudiar ${lenguaje}? Responde con el numero 1 para SI o 2 para NO: `))
+console.log(interaccion1)
+    if (interaccion1 == 1){
+        alert("Muy Bien! Sigue estudiando y tendras mucho exito.")
+        }    else if (interaccion1 == 2) {
+            alert("Lástima, tal vez haya otro lenguaje que te interese...")
+        }   else {
+            alert("No entendi tu respuesta..")
+        }
+
+
